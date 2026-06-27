ea5f1edc6d95b45b7227e874e8ad6ee7f9eb34fd
HAMZA SATOUT <hamzast2003@gmail.com>
2026-06-27 22:26:47 +0100

    disable CSR bailout

--- next.config.js
+++ next.config.js
@@ -1,5 +1,8 @@
 /** @type {import('next').NextConfig} */
 const config = {
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   images: {
     remotePatterns: [
       { protocol: 'https', hostname: '*.public.blob.vercel-storage.com' },
